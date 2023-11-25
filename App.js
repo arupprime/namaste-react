88117c234ba74d8c2101423549d89e348a8bff58
arupprime <arup.060101@gmail.com>
2023-11-25 18:35:10 +0530

    inception

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {}, "namaste react");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    React.createElement("h1", {}, "i am react hero")
+  )
+);
+
+root.render(parent);
